fb5c2a6e28be4f08e174fbea1f8838950a1cfa7b
Shakhod <shakhzod.nematullayev@ssd.uz>
2023-10-30 12:37:35 +0500

    create IFileModel interface

---
+++ FileService.Infrastructure/Interfaces/IFileModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FileService.Infrastructure.Interfaces
+{
+    public interface IFileModel
+    {
+        [Required]
+        string Name { get; set; }
+        byte[] Content { get; set; }
+        string Extension { get; set; }
+        string MimeType { get; set; }
+        [Required]
+        long Size { get; set; }
+    }
+}
---
+++ FileService.Infrastructure/Models/File.cs
@@ -0,0 +1,14 @@
+﻿using FileService.Infrastructure.Interfaces;
+
+namespace FileService.Infrastructure.Models
+{
+#pragma warning disable
+    public class File : IFileModel
+    {
+        public string MimeType { get; set; }
+        public string Name { get; set; }
+        public byte[] Content { get; set; }
+        public string Extension { get; set; }
+        public long Size { get; set; }
+    }
+}
