56bc6019c43e90da182cb7b520d4fe4acb4d5148
Shaxzod1311 <sh.nematullayev@baik.uz>
2023-10-31 17:31:54 +0500

    setting up serilog configuration

--- FileService.Application/Program.cs
+++ FileService.Application/Program.cs
@@ -1,3 +1,7 @@
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +12,26 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile(
+        $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+        optional: true)
+    .Build();
+
+var logger = new LoggerConfiguration()
+.Enrich.FromLogContext()
+.WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+{
+    AutoRegisterTemplate = true,
+    IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower()}-{DateTime.UtcNow:yyyy-MM}"
+})
+.Enrich.WithProperty("Environment", environment)
+.CreateLogger();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
