115b15c4c4dacc627f7cc83d1bf0df47fcd45c6e
Shaxzod1311 <sh.nematullayev@baik.uz>
2023-10-31 16:31:32 +0500

    setup service extensions

---
+++ FileService.Application/Extensions/ServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using FileService.Application.Data;
+using Microsoft.EntityFrameworkCore;
+namespace FileService.Application.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<FileServiceDb>(options =>
+            {
+                options.UseNpgsql(configuration.GetValue<string>("Database:ConnectionStrings:Postgres"));
+            });
+        }
+    }
+}
